4a194b5572886cb73708c0c46aa5b9eb3dffbb0e
vlad sheydenkov <vladsheydenkov@gmail.com>
2020-11-21 15:08:39 +0300

    add chapter_12

---
+++ chapter_12_if.py
@@ -0,0 +1,12 @@
+# multiple branching realisation using dict
+choice = 'garlic'
+print({'garlic': 1.0, 'orange': 2.0}[choice])
+
+# the same thing we can get using if/elif
+# redundant comments need in education purpose
+
+
+
+
+
+
